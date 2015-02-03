3e21ae1c74f40482a31dd9423ed99ea4c96e862f
soedar <soedar.sg@gmail.com>
2015-02-03 19:40:57 +0800

    Added vimperator configuration

--- setup.sh
+++ setup.sh
@@ -8,7 +8,6 @@ source zsh/setup.sh
 
 # tmux
 source tmux/setup.sh
-ln -s ~/.soedy/tmux.conf ~/.tmux.conf
 
-# clone submodule
-git submodule update --init
+# vimperator
+source vimperator/setup.sh
---
+++ vimperator/setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ln -s ~/.soedy/vimperator/vimperatorrc ~/.vimperatorrc
+ln -s ~/.soedy/vimperator ~/.vimperator
