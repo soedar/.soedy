5e2afb7e7bceeeccf121c7ea8dc615fe57f3159e
Soedarsono <soedar.sg@gmail.com>
2015-12-14 21:05:06 +0800

    Updated i3 configs

--- i3/setup.sh
+++ i3/setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+mkdir -p ~/.config
 ln -s ~/.soedy/i3/i3 ~/.config
 ln -s ~/.soedy/i3/i3status ~/.config
 ln -s ~/.soedy/i3/dunst ~/.config
+ln -s ~/.soedy/i3/i3blocks ~/.config
 
 sudo ln -s ~/.soedy/i3/xcwd /usr/bin
 
