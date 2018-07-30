322be2d2b5ff1318891391d6cbe5017127f0cee5
Soedarsono <soedar.sg@gmail.com>
2018-07-30 20:39:56 +0800

    Add xrdp related config

--- i3xrdp/setup.sh
+++ i3xrdp/setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 mkdir -p ~/.config
+mkdir -p ~/.bin
+
 ln -s ~/.soedy/i3xrdp/i3 ~/.config
 ln -s ~/.soedy/i3xrdp/i3status ~/.config
 ln -s ~/.soedy/i3xrdp/dunst ~/.config
 ln -s ~/.soedy/i3xrdp/i3blocks ~/.config
 
-sudo ln -s ~/.soedy/i3xrdp/xcwd /usr/local/bin
+ln -s ~/.soedy/i3xrdp/xcwd ~/.bin
