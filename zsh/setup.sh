72bf95e141820216cc8e5831df5f4dc61edaaa2f
Soedarsono <soedar.sg@gmail.com>
2023-06-04 17:39:58 +0800

    Add fzf as plugin

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 ln -s .soedy/zsh/p10k.zsh ~/.p10k.zsh
 ln -s .soedy/zsh/zshrc ~/.zshrc
 
 # Needed for zsh-completion
 rm -f ~/.zcompdump; compinit
+
+# Install fzf
+./plugins/fzf/install --completion --key-bindings --no-update-rc
